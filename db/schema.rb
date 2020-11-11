cf6d10562901d60040b8d986036b584ad986f6d7
Corbett Winningham <Corbett Winningham>
2020-11-11 12:20:16 -0700

    adding microposts resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_193053) do
+ActiveRecord::Schema.define(version: 2020_10_28_191125) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
